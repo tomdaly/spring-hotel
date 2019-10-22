5d9a9a914970dc27193f033c60d5073763e16151
tomdaly <tomdaly@users.noreply.github.com>
2019-10-22 16:56:34 +0100

    Add deleteProfanity function and tests

--- src/test/java/com/tomdaly/hotel/business/service/ProfanityServiceTest.java
+++ src/test/java/com/tomdaly/hotel/business/service/ProfanityServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
@@ -15,6 +16,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.doNothing;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ProfanityServiceTest {
@@ -31,6 +34,22 @@ public class ProfanityServiceTest {
   public void testAddProfanity_shouldReturnNewProfanityWithCorrectWord() {
     Profanity mockProfanity = new Profanity("foobar");
     assertThat(profanityService.addProfanity("foobar"), is(equalTo(mockProfanity)));
+  }
+
+  @Test
+  public void testDeleteProfanity_shouldReturnTrueIfProfanityDeleted() {
+    Profanity mockProfanity = new Profanity("foobar");
+    given(profanityRepository.findByWord("foobar")).willReturn(mockProfanity);
+    doNothing().when(profanityRepository).delete(mockProfanity);
+    assertThat(profanityService.deleteProfanity("foobar"), is(equalTo(true)));
+  }
+
+  @Test
+  public void testDeleteProfanity_shouldDeleteProfanityFromRepository() {
+    Profanity mockProfanity = new Profanity("foobar");
+    given(profanityRepository.findByWord("foobar")).willReturn(null);
+    willThrow(DataIntegrityViolationException.class).given(profanityRepository).delete(mockProfanity);
+    assertThat(profanityService.deleteProfanity("foobar"), is(equalTo(false)));
   }
 
   @Test
